d210a77a2f437ba3d57e339e9b092f6919d39df9
Nathas18 <natanelhasbani@icloud.com>
2026-09-16 15:56:15 -0600

    Refresh PWA for retrospective Veset Haguf

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='luaj-tahara-pwa-v27';
+const CACHE='luaj-tahara-pwa-v28';
 const SIDUR_CACHE='luaj-sidur-text-v3';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./style.css','./theme-overrides.css','./dedication-overrides.css','./sidur-overrides.css','./app.js','./pwa-install.js','./icon.svg','./icon-maskable.svg',
