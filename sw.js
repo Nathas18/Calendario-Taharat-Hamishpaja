268a8374042ab4a6a1925de02913e468315fab4c
Nathas18 <natanelhasbani@icloud.com>
2026-09-15 22:08:34 -0600

    Refresh PWA for Jajamim contacts

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='luaj-tahara-pwa-v21';
+const CACHE='luaj-tahara-pwa-v22';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./style.css','./theme-overrides.css','./app.js','./pwa-install.js','./icon.svg','./icon-maskable.svg',
-  ...Array.from({length:26},(_,i)=>`./app.part${i+1}.txt`)
+  ...Array.from({length:27},(_,i)=>`./app.part${i+1}.txt`)
 ];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
